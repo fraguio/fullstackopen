6df31bfce280cac7d9a395ebba0355cf574be369
eddu fraguío <1526666+fraguio@users.noreply.github.com>
2025-11-19 09:00:26 +0100

    refactor(ex. 1.8): encapsulate statistics in a separate component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
+    </>
+  )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
@@ -8,17 +26,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{'give feedback'}</h1>
-      <button onClick={() => setGood(good + 1)}>{'good'}</button>
-      <button onClick={() => setNeutral(neutral + 1)}>{'neutral'}</button>
-      <button onClick={() => setBad(bad + 1)}>{'bad'}</button>
-      <h1>{'statistics'}</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {good + neutral + bad}</div>
-      <div>average {(good - bad) / (good + neutral + bad) || 0}</div>
-      <div>positive {(good / (good + neutral + bad)) * 100 || 0} %</div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
