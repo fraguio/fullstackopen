aa482c63d4d44113d19c41054b20d13b0f42321e
eddu fraguío <1526666+fraguio@users.noreply.github.com>
2025-11-20 13:11:48 +0100

    refactor(ex. 2.10): refactor phonebook into components

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -23,7 +26,7 @@ const App = () => {
         person => person.name.toLowerCase() === newName.toLowerCase(),
       )
     ) {
-      alert(`${newName} is already added to numberbook`)
+      alert(`${newName} is already added to phonebook`)
       clearForm()
       return
     }
@@ -55,28 +58,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with:{' '}
-        <input value={filter} onChange={handleFilterChange} />
-      </div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handlePersonChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {personsToShow.map(person => (
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>
-      ))}
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <h3>add a new</h3>
+      <PersonForm
+        addPerson={addPerson}
+        handleNumberChange={handleNumberChange}
+        handlePersonChange={handlePersonChange}
+        newName={newName}
+        newNumber={newNumber}
+      />
+      <h3>Numbers</h3>
+      <Persons personsToShow={personsToShow} />
     </div>
   )
 }
