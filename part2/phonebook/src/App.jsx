ad25c0136c5bc842576f58f0e100cf56c1958286
eddu fraguío <1526666+fraguio@users.noreply.github.com>
2025-11-20 11:26:56 +0100

    feat(ex. 2.7): Prevent adding duplicate names

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,6 +6,11 @@ const App = () => {
 
   const addPerson = event => {
     event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+      return
+    }
     const personObject = {
       name: newName,
     }
