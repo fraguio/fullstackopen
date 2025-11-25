fe93fcca586855a2cdd19df27d3714613b9768c9
eddu fraguío <1526666+fraguio@users.noreply.github.com>
2025-11-25 12:14:29 +0100

    refactor(ex. 2.13): extract persons service module for phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
-import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,9 +11,9 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => setPersons(response.data))
+    personService.getAll().then(initialPersons => {
+      setPersons(initialPersons)
+    })
   }, [])
 
   const clearForm = () => {
@@ -37,9 +37,9 @@ const App = () => {
       number: newNumber,
     }
 
-    axios.post('http://localhost:3001/persons', personObject).then(response => {
-      setPersons(persons.concat(response.data))
-      clearForm()
+    personService.create(personObject).then(returnedPerson => {
+      setPersons(persons.concat(returnedPerson))
+      clearForm('')
     })
   }
 
